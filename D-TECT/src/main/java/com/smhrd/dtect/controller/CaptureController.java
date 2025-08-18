d02fb275f03b40bb362ec28a04ed7f9fe43dd93d
tnvs99 <tnvs99@gmail.com>
2025-08-18 18:38:52 +0900

    update : 화면 자동 캡처 기능

---
+++ D-TECT/src/main/java/com/smhrd/dtect/controller/CaptureController.java
@@ -0,0 +1,13 @@
+package com.smhrd.dtect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CaptureController {
+
+    @GetMapping("/capture")
+    public String capturePage() {
+        return "capture/capture";
+    }
+}
